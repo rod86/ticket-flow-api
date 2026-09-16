c41e8e74d5972e9b2325bcd5ff67c29deb1c1a49
Sergi Rodriguez <sergi86@gmail.com>
2026-09-16 17:41:22 +0200

    setup code quality tools

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/bundles.php',
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@PHP82Migration' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder($finder)
+;
