de76064cdec8f81b94fb5e9b8f81c334b982b330
Sergi Rodriguez <sergi86@gmail.com>
2026-09-20 11:13:43 +0200

    request validator

---
+++ src/UI/Http/Request/Tickets/CreateTicketRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Http\Request\Tickets;
+
+// use Symfony\Component\Validator\Constraints as Assert;
+
+// TODO wip
+final readonly class CreateTicketRequest
+{
+}
