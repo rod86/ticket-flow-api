391b603d9b6e9808bb2696265813c017fdadf121
Sergi Rodriguez <sergi86@gmail.com>
2026-09-22 18:58:53 +0200

    implement ApiExceptionListener.php

--- src/UI/Http/EventListener/ApiExceptionListener.php
+++ src/UI/Http/EventListener/ApiExceptionListener.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 final readonly class ApiExceptionListener
 {
